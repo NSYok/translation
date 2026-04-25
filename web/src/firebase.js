fee2c7aaa8b8dc63a9b121fa14da3398951e450d
Nattawut Yok <nattawut_yok@hotmail.com>
2026-04-25 20:41:58 +0700

    chore: move Firebase config to .env variables

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCfpsfrqz27vR702HzPUxDdSflreOsib9s",
-  authDomain: "coa-database-by-nsyok.firebaseapp.com",
-  projectId: "coa-database-by-nsyok",
-  storageBucket: "coa-database-by-nsyok.firebasestorage.app",
-  messagingSenderId: "776176055557",
-  appId: "1:776176055557:web:7bda2e8ff7fa0b7bdeb36c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
